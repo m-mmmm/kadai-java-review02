d4ce51696b7a19d41602e606364333f6a375efdc
miki.kida <mikimikikk55@gmail.com>
2023-11-25 23:19:17 +0900

    L$C6

--- src/SevenNumber.java
+++ src/SevenNumber.java
@@ -4,7 +4,7 @@ public class SevenNumber {
 	public static void main(String[] args) {
 		// iの値が100より小さい間、処理を繰り返す
 		for (int i = 1; i< 100; i++) {
-			// iの値が7の倍数または7が入っている数の時　clap!　と表示
+			// iの値が7の倍数または7が入っている数の時
 			if(i % 7 ==0 || i % 10 == 7 || i / 10 == 7) {
 				// clap!と表示する
 				System.out.println("clap!");
